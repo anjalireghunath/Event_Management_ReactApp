4cebcb3e05223e14e2ad090cb2da8bd227e5ba37
anjalireghunath <anjalireghu98@gmail.com>
2022-05-24 21:17:58 +0530

    add header

--- src/components/AddEvents.js
+++ src/components/AddEvents.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Header from './Header'
 
 const AddEvents = () => {
     var [name,setName]=useState("")
@@ -12,7 +13,7 @@ const AddEvents = () => {
     }
   return (
     <div>
-
+<Header/>
 
 <div className="container">
     <div className="row">
