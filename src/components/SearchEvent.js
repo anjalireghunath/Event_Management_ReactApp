46cf5e6f4150ed6f9f54c6951a2be4ed6314b841
anjalireghunath <anjalireghu98@gmail.com>
2022-05-24 21:06:30 +0530

    search_event

---
+++ src/components/SearchEvent.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+const SearchEvent = () => {
+    var [dates,setDates]=useState("")
+    const searchData=()=>{
+        const data={"date":dates}
+        console.log(data)
+    }
+  return (
+    <div>
+
+<div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="form-label">Date</label>
+                    <input onChange={(a)=>{setDates(a.target.value)}} type="date" name="" id="" className="form-control"/>
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <button onClick={searchData} className="btn btn-primary">SEARCH</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    </div>
+  )
+}
+
+export default SearchEvent
